6aadf10f00de6b0e8290b8b2cf503a57291e991d
Harry Anthony <anthonyharry@yahoo.com>
2024-02-11 20:03:08 +0000

    fith automation account CICD test

--- terraform-manifests/c7-07-iis-vm-resource .tf
+++ terraform-manifests/c7-07-iis-vm-resource .tf
@@ -111,7 +111,7 @@ SETTINGS
 }
 */
 
-# Generate random text for a unique storage account name
+# Generate random text for a unique storage account names
 resource "random_id" "iis1_random_id" {
   keepers = {
     # Generate a new ID only when a new resource group is defined
